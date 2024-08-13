ff7ec24d64f45d6ce68af71158f358a5b4b38494
Gab2903 <gabriel-koeppen@gmx.de>
2024-08-13 13:21:58 +0200

    merge home + fav

--- main.js
+++ main.js
@@ -7,13 +7,19 @@ const options = {
   },
 };
 
+{
+  /* <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+<path fill="currentColor" d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1" />
+</svg> */
+}
+
 const movieEl = document.getElementById("movies");
 
 const renderMovies = (movies) => {
   movies.forEach((element) => {
     const html = `
-       <div class="flex flex-col items-center bg-[#242d42] text-white rounded-lg overflow-hidden shadow-lg">
-        <img class="w-full" src="https://image.tmdb.org/t/p/w300${element.poster_path}" alt="${element.title}">
+       <div class="flex flex-col bg-[#242d42] text-white rounded-lg overflow-hidden shadow-lg hover:bg-[#374151]">
+        <img class="w-full hover:grayscale" src="https://image.tmdb.org/t/p/w500${element.poster_path}" alt="${element.title}">
         <div class="py-2 px-3">
           <p class="text-xs">${element.release_date}</p>
           <h2 class="text-base my-2">${element.title}</h2>
